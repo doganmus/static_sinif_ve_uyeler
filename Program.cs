a5a79e99a3566d9bcf86b0fd7c204ab4e19a834b
Dogan Mus <doganmus87@gmail.com>
2022-05-09 22:19:52 +0300

    proje il oluşturma

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace static_sinif_ve_uyeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
